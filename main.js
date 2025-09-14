9e7a7718f15dfc569e746aedb048039cac69b5f1
sraveshnandan <sraveshnandan@gmail.com>
2025-09-14 22:54:52 +0530

    loops cleraed

--- main.js
+++ main.js
@@ -2,27 +2,27 @@ console.log("This is the new log!");
 
 // variable and datatypes
 
-var a;
+// var a;
 
-a = 10;
-a = 12.65;
-a = "c";
-a = "String";
-a = true;
-a = undefined;
-a = null;
+// a = 10;
+// a = 12.65;
+// a = "c";
+// a = "String";
+// a = true;
+// a = undefined;
+// a = null;
 
-console.log(a);
+// console.log(a);
 
 // let , var , const
 
-let b = 10;
+// let b = 10;
 
-console.log("Value of b is: ", b);
+// console.log("Value of b is: ", b);
 
-b = 20;
+// b = 20;
 
-console.log("Value of b is: ", b ** 2);
+// console.log("Value of b is: ", b ** 2);
 
 // const c = 30;
 // console.log("Value of c is: ", c);
@@ -31,37 +31,102 @@ console.log("Value of b is: ", b ** 2);
 
 // arithmetic operators  +, - ,*, /, %, **
 
-let h = 8;
-let g = 7;
+// let h = 8;
+// let g = 7;
 
-console.log("h + g = ", h + g);
-console.log("h - g = ", h - g);
-console.log("h * g = ", h * g);
-console.log("h / g = ", h / g);
-console.log("h % g = ", h % g);
-console.log("h ** g = ", h ** g);
+// console.log("h + g = ", h + g);
+// console.log("h - g = ", h - g);
+// console.log("h * g = ", h * g);
+// console.log("h / g = ", h / g);
+// console.log("h % g = ", h % g);
+// console.log("h ** g = ", h ** g);
 
 // Assignment operators  =, +=, -=, *=, /=, %=, **=
-console.log("h++=", h++);
-console.log("h--=", h--);
-console.log("++h=", ++h);
-console.log("--h=", --h);
+// console.log("h++=", h++);
+// console.log("h", ++h);
+// console.log("h--=", h--);
+// // console.log("++h=", ++h);
+// console.log("--h=", --h);
 
 //relational operators  >, <, >=, <=, ==, ===, !=, !==
 
 //logical operators  &&, ||, !
 
-let age = 22;
+// && 1 1 = true
+// && 1 0 = false
+// && 0 1 = false
+// && 0 0 = false
 
-if (age >= 18 && age != 20) {
-  console.log("You can vote");
+// || 1 1 = true
+// || 1 0 = true
+// || 0 1 = true
+// || 0 0 = false
 
-  if (age === 22) {
-    console.log("Your age is 22");
-  }
-} else {
-  console.log("You cannot vote");
-}
+// ! 1 = false
+// ! 0 = true
+
+// let age = 22;
+
+// if (age >= 18 && age != 20) {
+//   console.log("You can vote");
+
+//   if (age === 22) {
+//     console.log("Your age is 22");
+//   }
+// } else {
+//   console.log("You cannot vote");
+// }
 
 // ternary operator
-age > 18 ? console.log("You can vote") : console.log("You cannot vote");
+// age > 18 ? console.log("You can vote") : console.log("You cannot vote");
+
+// Control Statement
+
+// if , if else , if else if , switch case
+
+// let num = 5;
+
+// if (num > 0) {
+//   console.log("Number is positive");
+// } else if (num < 0) {
+//   console.log("Number is negative");
+// } else {
+//   console.log("Number is zero");
+// }
+
+// switch case
+
+// let day = 2;
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   default:
+//     console.log("Invalid day");
+//     break;
+// }
+
+// loops
+
+// for  of , for in
+
+let arr1 = [10, 20, 30, 40];
+console.log("Length of arr1 is ", arr1.length);
+
+for (let i = 0; i < arr1.length; i++) {
+  console.log("arr1[i]: ", arr1[i + 2]);
+}
+
+for (const element of arr1) {
+  console.log("element: ", element);
+}
+
+for (const element in arr1) {
+  console.log("element: ", element);
+}
+
+// Q . Write a program to print the  table of from 1 to 20 using for loop.
